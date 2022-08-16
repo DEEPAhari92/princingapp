e8da10dad63c8a8ff2a5d55755458b5eb2ac1cc1
DEEPAhari92 <deepaeswari1992@gmail.com>
2022-08-16 12:25:08 +0530

    prising

--- src/App.js
+++ src/App.js
@@ -1,24 +1,168 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from "./Card";
+import "bootstrap/dist/css/bootstrap.min.css"
+import userEvent from '@testing-library/user-event';
+
 
 function App() {
+  const data = [
+    {
+      name : "Free",
+      price : 0,
+      period : "month",
+      features : [
+        {
+          title : "Single User" ,
+          isEnable : true
+
+        },
+        {
+          title : "5GB Storage" ,
+          isEnable : true
+
+        },
+        {
+          title : "Unlimited Public Projects" ,
+          isEnable : true
+
+        },
+        {
+          title : "Community Access" ,
+          isEnable : true
+
+        },
+        {
+          title : "Unlimited Private Projects" ,
+          isEnable :false
+
+        },
+        {
+          title : "Dedicated Phone Support" ,
+          isEnable :false
+
+        },
+        {
+          title : "Free Subdomain" ,
+          isEnable : false
+
+        },
+        {
+          title : "Monthly Status Reports" ,
+          isEnable : false
+
+        }
+      ]
+    },
+    {
+      name : "Plus",
+      price : 9,
+      period : "month",
+      features : [
+        {
+          title : "5 Users" ,
+          isEnable : true,
+          isBold : true
+
+        },
+        {
+          title : "50GB Storage" ,
+          isEnable : true
+
+        },
+        {
+          title : "Unlimited Public Projects" ,
+          isEnable : true
+
+        },
+        {
+          title : "Community Access" ,
+          isEnable : true
+
+        },
+        {
+          title : "Unlimited Private Projects" ,
+          isEnable : true
+
+        },
+        {
+          title : "Dedicated Phone Support" ,
+          isEnable : true
+
+        },
+        {
+          title : "Free Subdomain" ,
+          isEnable : true
+
+        },
+        {
+          title : "Monthly Status Reports" ,
+          isEnable :false
+
+        }
+      ]
+    },
+    {
+      name : "Pro",
+      price : 49,
+      period : "month",
+      features : [
+        {
+          title : "Unlimited User" ,
+          isEnable : true,
+          isBold : true
+
+        },
+        {
+          title : "150GB Storage" ,
+          isEnable : true
+
+        },
+        {
+          title : "Unlimited Public Projects" ,
+          isEnable : true
+
+        },
+        {
+          title : "Community Access" ,
+          isEnable : true
+
+        },
+        {
+          title : "Unlimited Private Projects" ,
+          isEnable : true
+
+        },
+        {
+          title : "Dedicated Phone Support" ,
+          isEnable : true
+
+        },
+        {
+          title : "Free Subdomain" ,
+          isEnable : true
+
+        },
+        {
+          title : "Monthly Status Reports" ,
+          isEnable : true
+
+        }
+      ]
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section className="pricing py-5">
+  <div className="container">
+    <div className="row">
+      {
+        data.map((item)=>{
+return <Card item = {item}></Card>
+        })
+      }
     </div>
+    </div>
+    </section>
   );
 }
 
